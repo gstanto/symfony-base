ae1b671543711ce52e578becc69ad955b3536543
Gary Stanton <wfm.gary.stanton@gmail.com>
2018-03-20 10:24:16 -0400

    added twig support to symfony base

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,16 +9,19 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController
+class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="app_home")
      */
     public function index()
     {
-        return new Response('Ok, Yes I am a base home Controller, baby, come again, no');
+        $name = "Grouper";
+        return $this->render('home/main.html.twig', ['name' => $name]);
     }
+
 }
